499dfcdd49192fc46d642a48f3a053719804b860
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-08-25 12:19:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-/home/adminuser/venv/bin/python -m pip install --upgrade pip
-/home/adminuser/venv/bin/python -m pip install pvlib
- /home/adminuser/venv/bin/python -m pip install pandas
+#/home/adminuser/venv/bin/python -m pip install --upgrade pip
+#/home/adminuser/venv/bin/python -m pip install pvlib
+# /home/adminuser/venv/bin/python -m pip install pandas
 import numpy as np
 import pandas as pd
 import pvlib
