e781eb28e03d96c8b7309382cf85f8a791bd43d4
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-05 18:35:06 +0000

    w

--- pages/lcoe.py
+++ pages/lcoe.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("Calculos económicos")
 
 if "my_input" in st.session_state:
-    st.write("Según lo simulado ",st.session_state["my_input"])
+    st.success("Según lo simulado ",st.session_state["my_input"])
 else:
     st.write("Primero simule un sistema fotovoltaico")
