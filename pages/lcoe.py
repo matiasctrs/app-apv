5b41dbc8afe7710b6e5e1a5eaa157bf244be39e7
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-05 19:57:19 +0000

    q

--- pages/lcoe.py
+++ pages/lcoe.py
@@ -6,6 +6,6 @@ st.title("Calculos económicos")
 if "resultado" in st.session_state:
     st.success(st.session_state["resultado"])
     st.write(st.session_state["azimuth"])
-    st.writw(st.session_state["pvrow_tilt"])
+    st.write(st.session_state["pvrow_tilt"])
 else:
     st.write("Primero simule un sistema fotovoltaico")
