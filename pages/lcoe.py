b613606aea6d32f482ff707c80afe2df44c6f204
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-05 18:31:33 +0000

    a

--- pages/lcoe.py
+++ pages/lcoe.py
@@ -6,4 +6,4 @@ st.title("Calculos económicos")
 if "my_input" in st.session_state:
     st.write("La generación fotovoltaica con la simulación anterior es: ",st.session_state["my_input"])
 else:
-    st.writw("Primero simule un sistema fotovoltaico")
+    st.write("Primero simule un sistema fotovoltaico")
