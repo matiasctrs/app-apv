d375d83def5e82445de7665cdcefdb7e3522e20b
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-05 18:31:01 +0000

    warning fix in lcoe

--- pages/lcoe.py
+++ pages/lcoe.py
@@ -3,4 +3,7 @@ import streamlit as st
 
 st.title("Calculos económicos")
 
-st.write("La generación fotovoltaica con la simulación anterior es: ",st.session_state["my_input"])
+if "my_input" in st.session_state:
+    st.write("La generación fotovoltaica con la simulación anterior es: ",st.session_state["my_input"])
+else:
+    st.writw("Primero simule un sistema fotovoltaico")
