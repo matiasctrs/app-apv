a05c9c41f1266e70ec1da6656fd9ce623f0f89de
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-05 18:19:37 +0000

    input compartido

--- app.py
+++ app.py
@@ -96,8 +96,16 @@ if simular:
     else:
         st.write("Ingrese coordenadas válidas")
    
-   
+ ##-------GUARDA INPUT EN LA SESION PARA OCUPAR EN OTRAS PAGINAS------
 
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.success(formula_en_bloque)
+
+if simular:
+    st.session_state["my_input"] = my_input
 
 #----CONTACT----
 
---
+++ pages/lcoe.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Calculos económicos")
+
+st.write("La generación fotovoltaica con la simulación anterior es: ",st.session_state["my_input"])
