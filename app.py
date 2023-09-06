187480c883d1b1553a1bd628e0d18815132f7571
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-06 19:31:07 +0000

    asdsa

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ folium.Marker([-15.087836, -44.015762], tooltip="Ubicación seleccionada").add_t
 st_folium(m)
 
 
+st.write("las coordenadas seleccionadas son: ", popup1.getLat())
 # definición de variables
 
 st.session_state["Latitude"]= st.number_input("Ingresa la latitud", value = -15.087836, format = "%.6f")
