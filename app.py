bc91cd0e984854ad578413da2b8c057ec1b0525d
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-06 15:39:00 +0000

    ASDAS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import importlib
 #import datetime
 #import pytz
 import av_utils as av
-import streamlit_folium as sf
+from streamlit_folium import folium_static
 import folium
 importlib.reload(av)
 
