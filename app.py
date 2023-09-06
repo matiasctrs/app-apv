dfb3f6c68c3cef212469f8d126d194e0c5c59c3d
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-06 20:17:01 +0000

    asd

--- app.py
+++ app.py
@@ -32,44 +32,15 @@ m.add_child(popup1)
 # Agregar un control de clic en el mapa
 folium.Marker([-15.087836, -44.015762], tooltip="Ubicación seleccionada").add_to(m)
 
-####prueba
-
-class LatLngPopup(MacroElement):
-    """
-    When one clicks on a Map that contains a LatLngPopup,
-    a popup is shown that displays the latitude and longitude of the pointer.
-
-    """
-    _template = Template(u"""
-            {% macro script(this, kwargs) %}
-                var {{this.get_name()}} = L.popup();
-                function latLngPop(e) {
-                data = e.latlng.lat.toFixed(4) + "," + e.latlng.lng.toFixed(4);
-                    {{this.get_name()}}
-                        .setLatLng(e.latlng)
-                        .setContent( "<br /><a href="+data+"> click </a>")
-                        .openOn({{this._parent.get_name()}})
-                    }
-                {{this._parent.get_name()}}.on('click', latLngPop);
-
-            {% endmacro %}
-            """)  # noqa
-
-    def __init__(self):
-        super(LatLngPopup, self).__init__()
-        self._name = 'LatLngPopup'
 
 
-
-
-#####
 
 
 
 # Mostrar el mapa en Streamlit
 st_folium(m)
-st.write(folium.LatLng)
 
+  
 
 #st.write("las coordenadas seleccionadas son: ", popup1.)
 # definición de variables
