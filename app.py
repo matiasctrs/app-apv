ffd0b9d75f89473dfdf495e6524d04259663ddf3
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-06 12:58:47 +0000

    w

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ if "resultado" in st.session_state:
 
 for variable in st.session_state:
     #if len(variable) < 3:
-    st.session_state["resumen"] = pd.DataFrame(variable, columns =¨["Vaiable", "Valor"])
+    st.session_state["resumen"] = pd.DataFrame(variable, columns =["Variable", "Valor"])
 
 
 
