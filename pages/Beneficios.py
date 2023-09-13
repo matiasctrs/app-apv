9d8452ad3fe5488c7e2c4496985750474c54ce3e
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-13 13:49:29 +0000

    beneficios

---
+++ pages/Beneficios.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import av_utils as av
+
+st.title("Cálculo de beneficios económicos")
+st.header("Resumen de costos económicos: ")
+if "LCOE" in st.session_state:
+    st.success(st.session_state["LCOE"])
+else:
+    st.write("Primero simule el costo del proyecto")
