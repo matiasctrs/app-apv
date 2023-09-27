ccf02fc35c2e09812ca155d5e7cc202fee03d446
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-27 17:34:29 -0300

    trys

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ st.subheader("Simulación eléctrica:")
 
 img = image_select(
     label = "Selecciona un diseño",
-    images = [Image.open("tracking.png"),
+    images = ["main/tracking.png",
               "https://github.com/matiasctrs/app-apv/blob/main/vertical.png"],
     captions=["Tracking", "Vertical"],
 )
