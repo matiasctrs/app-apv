8265d4a6b5e706ae04e68378d7da641c35b764d0
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-28 14:46:29 -0300

    sss

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ for image in images:
 
     # Redimensiona la imagen
     width, height = image.size
-    new_width = 460  # Define el nuevo ancho deseado
-    new_height = 450 #int(height * (new_width / width))
+    new_width = 617 # Define el nuevo ancho deseado
+    new_height = 617 #int(height * (new_width / width))
     resized_image = image.resize((new_width, new_height))
     images_r.append(resized_image)
 
