06dfe924114fa179bf14ca989ca9f2a7145c035a
matiasctrs <136755199+matiasctrs@users.noreply.github.com>
2023-09-28 09:40:01 -0300

    fix width

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ img = image_select(
     images = ["https://github.com/matiasctrs/app-apv/blob/main/tracking.png?raw=true",
               "https://github.com/matiasctrs/app-apv/blob/main/vertical_new.png?raw=true"],
     captions=["Tracking", "Vertical"],
-    use_container_width=False,
+    use_container_width=True,
 )
 
 Track_options = [True,False]
